f7b092ab3c4d4657a49a1a52f945cb9da25ebedc
Alejandro <star.trip.64@gmail.com>
2024-11-25 14:05:37 -0600

    subir proyecto

---
+++ Desktop/Don Galleto/src/test/java/com/idgs/DonGalletoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idgs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonGalletoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
